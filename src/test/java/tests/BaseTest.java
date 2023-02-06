347fadd1b937b162331c71fff679189a9fdfe9a5
ajjankowski <aj.jankowski92@gmail.com>
2023-02-06 19:44:26 +0100

    Initial commit and base test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected static WebDriver driver;
+
+    @Test
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://mennicakapitalowa.pl/Skup-ccms-pol-32.html");
+        driver.quit();
+    }
+}
