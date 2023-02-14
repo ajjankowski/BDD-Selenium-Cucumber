87efcbbcd26b18dcfb2eb6f32434089cb5bfca21
ajjankowski <aj.jankowski92@gmail.com>
2023-02-14 19:19:27 +0100

    24h availability gone fix

--- src/test/java/pages/GoldenmarkPage.java
+++ src/test/java/pages/GoldenmarkPage.java
@@ -22,8 +22,14 @@ public class GoldenmarkPage {
     @FindBy(xpath = "//input[@title=\"1 dzień\"]")
     private WebElement oneDayRadioButton;
 
+    @FindBy(xpath = "//input[@title=\"9 dni\"]")
+    private WebElement nineDayRadioButton;
+
+    @FindBy(xpath = "//input[@title=\"45 dni\"]")
+    private WebElement fortyFiveDayRadioButton;
+
     public void checkGoldenmarkPrices(HashMap<String, String> goldenmarkCoinsList) throws InterruptedException {
-        System.out.println("Goldenmark price of coins 24h:");
+        System.out.println("Goldenmark coin prices:");
         for (String coinName : goldenmarkCoinsList.keySet()) {
             openWeb(goldenmarkCoinsList.get(coinName));
             checkRadioButton();
@@ -33,19 +39,34 @@ public class GoldenmarkPage {
 
     public void openWeb(String link) {
         driver.get(link);
-        Assert.assertEquals(driver.getCurrentUrl(), link);
+//        Assert.assertEquals(driver.getCurrentUrl(), link);
     }
 
     public void checkRadioButton() {
-        if (!oneDayRadioButton.isSelected()) {
-            oneDayRadioButton.click();
+        try {
+            if (!oneDayRadioButton.isSelected()) {
+                oneDayRadioButton.click();
+            }
+            System.out.println("Coin available in 24h");
+        } catch (Exception e) {
+            try {
+                if (!nineDayRadioButton.isSelected()) {
+                    nineDayRadioButton.click();
+                }
+                System.out.println("Coin available in 9 days");
+            } catch (Exception e2) {
+                if (!fortyFiveDayRadioButton.isSelected()) {
+                    fortyFiveDayRadioButton.click();
+                }
+                System.out.println("Coin available only in 45 days");
+            }
         }
     }
 
     public void checkPrice(String coinName, String link) throws InterruptedException {
         Thread.sleep(1000);
         String coinPrice = goldenmarkCoinPrice.getAttribute("content");
-        String logInfo = coinPrice + " - " + coinName + " " + link;
+        String logInfo = "-> " + coinPrice + " - " + coinName + " " + link;
         System.out.println(logInfo);
     }
 }
--- src/test/java/tests/GoldMonitorTest.java
+++ src/test/java/tests/GoldMonitorTest.java
@@ -16,9 +16,9 @@ public class GoldMonitorTest extends BaseTest {
 
 // TODO: Set names and links from goldenmark.com to desired coins
         HashMap<String, String> goldenmarkCoinsList = new HashMap<>();
-        goldenmarkCoinsList.put("Kangaroo", "https://goldenmark.com/pl/australijski-kangur-1-uncja-zlota/#/282-cennik-1_dzien");
-        goldenmarkCoinsList.put("Leaf", "https://goldenmark.com/pl/kanadyjski-lisc-klonowy-1-uncja-zlota/#/282-cennik-1_dzien");
-        goldenmarkCoinsList.put("Britannia", "https://goldenmark.com/pl/britannia-1-uncja-zlota-2929/#/282-cennik-1_dzien");
+        goldenmarkCoinsList.put("Kangaroo", "https://goldenmark.com/pl/australijski-kangur-1-uncja-zlota/");
+        goldenmarkCoinsList.put("Leaf", "https://goldenmark.com/pl/kanadyjski-lisc-klonowy-1-uncja-zlota/");
+        goldenmarkCoinsList.put("Britannia", "https://goldenmark.com/pl/britannia-1-uncja-zlota-2929/");
         new GoldenmarkPage(BaseTest.driver).checkGoldenmarkPrices(goldenmarkCoinsList);
     }
 }
