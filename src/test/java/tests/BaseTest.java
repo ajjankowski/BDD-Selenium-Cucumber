2496d54045e326d435e4573f2dae621cd12c0e72
ajjankowski <aj.jankowski92@gmail.com>
2023-02-15 15:08:12 +0100

    WebDriver manager added in BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,8 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -13,9 +15,28 @@ public class BaseTest {
 
     @BeforeSuite
     public void setUp() {
-        System.out.println("Setting up chromedriver");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+// TODO: Set browser
+        String driverType = "Chrome";
+        System.out.println("Starting feature in " + driverType + " driver");
+        switch (driverType.toLowerCase()) {
+            case "chrome" -> {
+                io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                driver.manage().window().maximize();
+            }
+            case "firefox" -> {
+                io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                driver.manage().window().maximize();
+            }
+            case "edge" -> {
+                io.github.bonigarcia.wdm.WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+
+            }
+            default -> System.out.println("Wrong driver type: use Chrome, Firefox or Edge");
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
