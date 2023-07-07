5bb13e6fc496c5ba0c077fdb71fc568ea285fcaf
ajjankowski <aj.jankowski92@gmail.com>
2023-07-07 11:31:10 +0200

    New file structure

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "@coins",
+        features = "src/test/resources",
+        glue = "steps",
+        plugin = {"pretty", "html:target/cucumber-report/cucumber.html",
+                "json:target/cucumber-report/cucumber.json",
+                "junit:target/cucumber-report/cucumber.xml"})
+public class TestRunner {
+}
